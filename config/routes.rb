e395ee4c7d05647f0336484cf21c206bf229ac52
rvincelee <vincelee_05@yahoo.com>
2023-11-28 20:25:31 -0600

    Created basic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root 'home#index'
 
   get 'home/index'
-  resources :genre, only: %i[index show]
+
+  resources :genres, only: %i[index show]
   resources :order_details, only: %i[index show]
-  resources :order, only: %i[index show]
-  resources :user, only: %i[index show]
-  resources :author, only: %i[index show]
-  resources :manga, only: %i[index show]
+  resources :orders, only: %i[index show]
+  resources :users, only: %i[index show]
+  resources :authors, only: %i[index show]
+  resources :mangas, only: %i[index show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get "pages/:name", to: "pages#show", as: "page"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
