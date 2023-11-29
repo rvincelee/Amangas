f209066de09b5502142f4062981a53fa0cd1f0bc
rvincelee <vincelee_05@yahoo.com>
2023-11-28 20:40:29 -0600

    Added navigation throughout links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    manga_controller = MangaController.new
+    manga_controller = MangasController.new
     manga_controller.index
     @mangas = manga_controller.instance_variable_get(:@mangas)
   end
