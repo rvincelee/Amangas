4a53dd6861c90901afa86e8d3ab9382985fc5c6f
To <phucto91@gmail.com>
2023-11-29 21:40:11 -0600

    Added tailwind, designed pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,62 +2,62 @@ require "rest-client"
 require "json"
 require "faker"
 
-# AdminUser.delete_all
-# OrderDetail.delete_all
-# Order.delete_all
-# Manga.delete_all
-# User.delete_all
-# Author.delete_all
-# Genre.delete_all
-# Province.delete_all
+AdminUser.delete_all
+OrderDetail.delete_all
+Order.delete_all
+Manga.delete_all
+User.delete_all
+Author.delete_all
+Genre.delete_all
+Province.delete_all
 
-# if Rails.env.development?
-#   AdminUser.create!(email: "admin@example.com", password: "password",
-#                     password_confirmation: "password")
-# end
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password",
+                    password_confirmation: "password")
+end
 
-# response = RestClient.get("https://api.jikan.moe/v4/genres/manga")
+response = RestClient.get("https://api.jikan.moe/v4/genres/manga")
 
-# genres_data = JSON.parse(response.body)
+genres_data = JSON.parse(response.body)
 
-# genres_data["data"].each do |genre|
-#   Genre.find_or_create_by(name: genre["name"])
-# end
+genres_data["data"].each do |genre|
+  Genre.find_or_create_by(name: genre["name"])
+end
 
-# sleep(1)
+sleep(1)
 
-# response = RestClient.get("https://api.jikan.moe/v4/manga?score>9.01")
+response = RestClient.get("https://api.jikan.moe/v4/manga?score>9.01")
 
-# manga_data = JSON.parse(response.body)
+manga_data = JSON.parse(response.body)
 
-# manga_data["data"].each do |manga|
-#   day = manga["published"]["prop"]["from"]["day"]
-#   month = manga["published"]["prop"]["from"]["month"]
-#   year = manga["published"]["prop"]["from"]["year"]
+manga_data["data"].each do |manga|
+  day = manga["published"]["prop"]["from"]["day"]
+  month = manga["published"]["prop"]["from"]["month"]
+  year = manga["published"]["prop"]["from"]["year"]
 
-#   author = Author.find_or_create_by(name: manga["authors"][0]["name"])
+  author = Author.find_or_create_by(name: manga["authors"][0]["name"])
 
-#   manga_instance = author.manga.find_or_create_by(
-#     title:        manga["title_english"],
-#     description:  manga["synopsis"],
-#     score:        manga["score"],
-#     price:        Faker::Commerce.price(range: 7.99..12.99),
-#     publish_date: Date.new(year, month, day),
-#     status:       manga["status"]
-#   )
-#   download_image = URI.parse(manga["images"]["webp"]["image_url"]).open
-#   manga_instance.image.attach(io:       download_image,
-#                               filename: "#{manga['title_english']}.webp")
+  manga_instance = author.manga.find_or_create_by(
+    title:        manga["title_english"],
+    description:  manga["synopsis"],
+    score:        manga["score"],
+    price:        Faker::Commerce.price(range: 7.99..12.99),
+    publish_date: Date.new(year, month, day),
+    status:       manga["status"]
+  )
+  download_image = URI.parse(manga["images"]["webp"]["image_url"]).open
+  manga_instance.image.attach(io:       download_image,
+                              filename: "#{manga['title_english']}.webp")
 
-#   next unless manga_instance.valid?
+  next unless manga_instance.valid?
 
-#   manga["genres"].each do |genre_data|
-#     genre = Genre.find_by(name: genre_data["name"])
-#     genre.mangas << manga_instance if !manga_instance.genres.exists?(genre.id) && manga_instance
-#   end
+  manga["genres"].each do |genre_data|
+    genre = Genre.find_by(name: genre_data["name"])
+    genre.mangas << manga_instance if !manga_instance.genres.exists?(genre.id) && manga_instance
+  end
 
-#   sleep(1)
-# end
+  sleep(1)
+end
 
 provinces = [{ name: "Alberta", abbreviation: "AB", PST: nil, GST: 0.05, HST: nil },
              { name: "British Columbia", abbreviation: "BC", PST: 0.07, GST: 0.05, HST: nil },
