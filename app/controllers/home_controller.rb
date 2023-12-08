4608f9e09b39c9a88e6b9305cce16132adfb7479
To <phucto91@gmail.com>
2023-12-08 16:00:56 -0600

    Styled pages, added home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,13 @@ class HomeController < ApplicationController
     session["checkout_address"] = nil
     session["order"] = nil
     session["order_details"] = nil
+
+    @mangas = Manga.all
+
+    @featured = @mangas.order(price: :asc).limit(5)
+
+    @best_sellers = @mangas.where("score > ?", 9).limit(5)
+
+    @completed = @mangas.where(status: "Finished").limit(5)
   end
 end
